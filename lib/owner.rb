dda0dcf642cfb89274bebca156697c7d412d2b26
Gabriel Valle <gvalle91@gmail.com>
2018-06-19 02:34:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -20,6 +20,16 @@ class Owner
     @pets[:fishes] << new_fish
   end
   
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
+  end
+  
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+  
   def say_species
     "I am a human."
   end
